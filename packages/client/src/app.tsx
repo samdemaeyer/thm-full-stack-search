905b49ca4c2ba43ca2d209e85b80ad771af8da09
Sofia <karaterzidi@live.com>
2024-09-20 18:09:33 +0300

    feature/create-models

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -6,18 +6,7 @@ import {
   fetchAndFilterCountries,
   fetchAndFilterCities,
 } from "./services/apiService";
-
-type Hotel = {
-  _id: string;
-  chain_name: string;
-  hotel_name: string;
-  city: string;
-  country: string;
-};
-
-type Country = { _id: string; country: string; countryisocode: string };
-
-type City = { _id: string; name: string };
+import { Hotel, Country, City } from "./types/models";
 
 function App() {
   const [hotels, setHotels] = useState<Hotel[]>([]);
---
+++ packages/client/src/types/models.ts
@@ -0,0 +1,18 @@
+export type Hotel = {
+  _id: string;
+  chain_name: string;
+  hotel_name: string;
+  city: string;
+  country: string;
+};
+
+export type Country = {
+  _id: string;
+  country: string;
+  countryisocode: string;
+};
+
+export type City = {
+  _id: string;
+  name: string;
+};
