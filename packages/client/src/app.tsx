a9cd284bc138a5c1fa1e905ae07c369cc845b27f
Sofia <karaterzidi@live.com>
2024-09-20 18:17:11 +0300

    feature/use-search-data-hook

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,45 +1,22 @@
 import { useState, type ChangeEvent } from "react";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ResultsList from "./components/ResultsList/ResultsList";
-import {
-  fetchAndFilterHotels,
-  fetchAndFilterCountries,
-  fetchAndFilterCities,
-} from "./services/apiService";
-import { Hotel, Country, City } from "./types/models";
+import useSearchData from "./hooks/useSearchData"; // Import your custom hook
 
 function App() {
-  const [hotels, setHotels] = useState<Hotel[]>([]);
-  const [countries, setCountries] = useState<Country[]>([]);
-  const [cities, setCities] = useState<City[]>([]);
   const [searchValue, setSearchValue] = useState("");
   const [showClearBtn, setShowClearBtn] = useState(false);
 
-  const fetchData = async (event: ChangeEvent<HTMLInputElement>) => {
+  const { hotels, countries, cities } = useSearchData(searchValue); // Use the hook
+
+  const fetchData = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setSearchValue(value);
     setShowClearBtn(!!value);
-
-    if (value === "") {
-      setHotels([]);
-      setCountries([]);
-      setCities([]);
-      return;
-    }
-
-    const filteredHotels = await fetchAndFilterHotels(value);
-    const filteredCountries = await fetchAndFilterCountries(value);
-    const filteredCities = await fetchAndFilterCities(value);
-    setHotels(filteredHotels);
-    setCountries(filteredCountries);
-    setCities(filteredCities);
   };
 
   const clearSearch = () => {
     setSearchValue("");
-    setHotels([]);
-    setCountries([]);
-    setCities([]);
     setShowClearBtn(false);
   };
 
