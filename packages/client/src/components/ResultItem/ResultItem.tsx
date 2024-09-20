8191ea52aab85383ad7ba8920c4f1eef0ffb2831
Sofia <karaterzidi@live.com>
2024-09-21 02:55:15 +0300

    feature/item-detail-page

--- packages/client/src/components/ResultItem/ResultItem.tsx
+++ packages/client/src/components/ResultItem/ResultItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Hotel, Country, City } from "../../types/models";
 import { getIconClass } from "../../utils/iconHelper";
+import { Link } from "react-router-dom";
 import "./ResultItem.css";
 
 type Item = Hotel | Country | City;
@@ -13,14 +14,16 @@ interface ResultItemProps {
 const ResultItem: React.FC<ResultItemProps> = React.memo(({ item, type }) => {
   return (
     <li key={item._id}>
-      <a href={`/${type}/${item._id}`} className="dropdown-item">
+      <Link to={`/${type}/${item._id}`} className="dropdown-item">
+        {" "}
+        {/* Use Link instead of a */}
         <i className={`fa ${getIconClass(type)} mr-2`}></i>
         {type === "hotels"
           ? (item as Hotel).hotel_name
           : type === "countries"
           ? (item as Country).country
           : (item as City).name}
-      </a>
+      </Link>
       <hr className="divider" />
     </li>
   );
